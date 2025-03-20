d0c6d87b2f78224ba0a297bd8a82d8b555636aad
Tfactor2020@gmail.com <Tfactor08>
2025-03-20 22:31:10 +0300

    add add_zeros function

--- task1.c
+++ task1.c
@@ -7,13 +7,34 @@ void go_through(node_t*);
 void free_array(node_t*);
 void add_element(node_t*, int, int);
 
+struct node_t {
+    int data;
+    node_t *next;
+};
+
+void add_zeros(node_t *ap)
+{
+    int i = 0;
+    
+    while (ap != NULL) {
+        if (ap->data < 0) {
+            add_element(ap, i+1, 0);
+        }
+        ap = ap->next;
+        i++;
+    }
+}
+
 /* create and process list S: S is a list of integers; after each negative add new zero element */
 int main()
 {
     node_t *ap = init_array(5);
     go_through(ap);
+
     putchar('\n');
-    add_element(ap, 5, 69);
+
+    add_zeros(ap);
+
     go_through(ap);
     free_array(ap);
 
