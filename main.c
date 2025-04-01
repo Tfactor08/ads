91f05b4003b13c69248a45ae140f78b0a5d86f73
Tfactor2020@gmail.com <Tfactor08>
2025-04-01 22:15:15 +0300

    add stack implementation

--- main.c
+++ main.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 
 typedef struct node_t node_t;
+typedef struct stack stack;
 
+// dynamic-array.c
 node_t* init_array(int n);
 void go_through(node_t*);
 void free_array(node_t*);
 void add_element(node_t*, int, int);
+int get_element(node_t*, int);
+
+// stack.c
+stack init_stack();
+int is_empty(stack *s);
+void push(stack *s, int);
+int pop(stack *s);
+
+struct stack {
+    node_t *data;
+    int top;
+};
 
 int main()
 {
-    node_t *ap = init_array(5);
-    go_through(ap);
-    putchar('\n');
-    add_element(ap, 5, 69);
-    go_through(ap);
-    free_array(ap);
+    stack s = init_stack();
+    push(&s, 69);
+    push(&s, 42);
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+    free_array(s.data);
 
     return 0;
 }
