39947e2ca321c75487a3eeef16770a76ac4b9918
Tfactor2020@gmail.com <Tfactor08>
2025-04-28 13:10:49 +0300

    add gen_queue

--- task2.c
+++ task2.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 typedef struct node_t node_t;
 typedef struct queue queue;
@@ -25,7 +27,16 @@ queue gen_queue(int);
 /* form a stack S containing all unique elements of queues L1 and L2 */
 int main()
 {
-        
+    srand(time(NULL));
+
+    queue l1 = gen_queue(10);
+    queue l2 = gen_queue(10);
+
+    for (int i = 0; i < 10; ++i)
+        printf("%d\t", dequeue(&l1));
+    putchar('\n');
+    for (int i = 0; i < 10; ++i)
+        printf("%d\t", dequeue(&l2));
 
     return 0;
 }
@@ -34,8 +45,6 @@ queue gen_queue(int n)
 {
     queue q = init_queue();
     for (int i = 0; i < n; ++i)
-    {
-        enqueue(&q, n);
-    }
+        enqueue(&q, rand() % 20);
     return q;
 }
