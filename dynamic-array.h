4ecc9bfdf47497f314a8b7d7309eb076953c29a0
Tfactor08 <Tfactor2020@gmail.com>
2025-05-02 17:14:51 +0300

    add header file for dynamic-array.c

---
+++ dynamic-array.h
@@ -0,0 +1,16 @@
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+
+typedef struct node_t node_t;
+
+struct node_t {
+    int data;
+    node_t *next;
+};
+
+node_t* init_array(int n);
+void go_through(node_t*);
+void free_array(node_t*);
+void add_element(node_t*, int, int);
+
+#endif
